ef478bd11de2dabb4535513a1e90c4fad9f5e265
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-28 16:43:41 +0000

    Adding first Java project for club management.

---
+++ 1-club-management-application/src/js_club/JS_Club.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package js_club;
+
+import javax.swing.*;
+import static javax.swing.JFrame.*;
+import js_club.aMain_Page.*;
+import js_club.bMemberShip.*;
+
+
+public class JS_Club {
+
+    public static void main(String[] args) {
+       mainWindow me = new mainWindow();
+        me.setVisible(true);
+    }
+    
+}
